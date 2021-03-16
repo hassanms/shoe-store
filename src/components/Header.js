098ffb55e8b3c12cbe1e7486bdaf5e6d5e616419
hassanms <hassansid95@gmail.com>
2021-03-16 17:30:51 +0500

    Add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,93 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PhoneIcon from '@material-ui/icons/Phone';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import HelpIcon from '@material-ui/icons/Help';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import { Link } from "react-router-dom";
 
-export default function Header() {
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
     return (
-        <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/nike">Nike</Link>
-                <Link to="/adidas">Adidas</Link>
-            </nav>
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-force-tabpanel-${index}`}
+        aria-labelledby={`scrollable-force-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      width: '100%',
+      backgroundColor: theme.palette.background.paper,
+    },
+    linkDecoration: {
+        textDecoration: "none",
+    }
+  }));
+
+export default function Header() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons="on"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="scrollable force tabs example"
+                >
+                    <Link to="/"><Tab label="Home" icon={<PhoneIcon />} /></Link>
+                    <Link to="/nike"><Tab label="Nike" icon={<FavoriteIcon />} /></Link>
+                    <Link to="/adidas"><Tab label="Adidas" icon={<PersonPinIcon />} /></Link>
+                    <Tab label="Item Four" icon={<HelpIcon />} />
+                </Tabs>
+            </AppBar>
+            {/* <TabPanel value={value} index={0}>
+                Home
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Nike
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Adidas
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel> */}
         </div>
     );
+    
 }

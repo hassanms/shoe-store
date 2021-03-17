ec8b48aa08429dea71032df2b70af3005170579c
hassanms <hassansid95@gmail.com>
2021-03-17 14:08:34 +0500

    Completed building navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,92 +1,48 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HelpIcon from '@material-ui/icons/Help';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import { ReactComponent as Home } from '../media/icons/home.svg';
+import { ReactComponent as Nike } from '../media/icons/nike.svg';
+import { ReactComponent as Adidas } from '../media/icons/adidas.svg';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`scrollable-force-tabpanel-${index}`}
-        aria-labelledby={`scrollable-force-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+  linkDecoration: {
+      textDecoration: "none",
+      color: "black"
+  },
+  selectedLink: {
+    color: "blue"
   }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-  };
-  
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      width: '100%',
-      backgroundColor: theme.palette.background.paper,
-    },
-    linkDecoration: {
-        textDecoration: "none",
-    }
-  }));
+}));
 
 export default function Header() {
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+    const [selected, setSelected] = useState(0);
 
     return (
         <div className={classes.root}>
             <AppBar position="static" color="default">
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    variant="scrollable"
-                    scrollButtons="on"
-                    indicatorColor="primary"
-                    textColor="primary"
-                    aria-label="scrollable force tabs example"
-                >
-                    <Link to="/"><Tab label="Home" icon={<PhoneIcon />} /></Link>
-                    <Link to="/nike"><Tab label="Nike" icon={<FavoriteIcon />} /></Link>
-                    <Link to="/adidas"><Tab label="Adidas" icon={<PersonPinIcon />} /></Link>
-                    <Tab label="Item Four" icon={<HelpIcon />} />
+                <Tabs>
+                    <Link to="/" className={clsx({[classes.linkDecoration]: true, [classes.selectedLink]: selected === 0})} onClick={() => setSelected(0)}>
+                      <Tab label="Home" icon={<Home width={30} height={30} />} />
+                      </Link>
+                    <Link to="/nike" className={clsx({[classes.linkDecoration]: true, [classes.selectedLink]: selected === 1})} onClick={() => setSelected(1)}>
+                      <Tab label="Nike" icon={<Nike width={30} height={30} />} />
+                      </Link>
+                    <Link to="/adidas" className={clsx({[classes.linkDecoration]: true, [classes.selectedLink]: selected === 2})} onClick={() => setSelected(2)}>
+                      <Tab label="Adidas" icon={<Adidas width={30} height={30} />} />
+                      </Link>
                 </Tabs>
             </AppBar>
-            {/* <TabPanel value={value} index={0}>
-                Home
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Nike
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                Adidas
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                Item Four
-            </TabPanel> */}
         </div>
     );
     
